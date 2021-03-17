1dcbbefaf229578ebbc8764330b6b17a8db190c3
Matias Sivertsvik <matiassi@stud.ntnu.no>
2021-03-17 15:55:43 +0100

    Add solution to task b + simple error handling

--- PE4/wish.c
+++ PE4/wish.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <errno.h>
 
 
 int main() {
@@ -17,9 +19,10 @@ int main() {
         chars = getline(&user_in, &input_lim, stdin);
         
         for (ap = argv; (*ap = strsep(&user_in, " ")) != NULL;) {
-            if (**ap != NULL) {
+            if (**ap != '\0') {
     //          The pointer at index i points to the array at *ap
                 argv[counter] = *ap;
+                
                 counter++;
                 if (++ap >= &argv[10]) {
                     break;
@@ -27,11 +30,29 @@ int main() {
             }
         }
         
+        // Parsing adds extra null char, so we need to remove it...
+        int temp = counter - 1;
+        argv[temp][strlen(argv[temp])-1] = 0;
         
-        
+        /*
         for (int i = 0; i < counter; i++) {
             printf("\nargv[%d] = %s", i, argv[i]);
         }
+        */
+        
+        char* binaryPath = argv[0];
+        int id = fork();
+
+        if (id == 0){
+            int error = execv(binaryPath, argv);
+            if (error == -1){
+                perror("An error occured when executing shell command\n");
+                printf( "Value of errno: %d\n", errno );
+            }
+            exit(3);
+        }
+
+        wait(0);
             
         free(user_in);
         
